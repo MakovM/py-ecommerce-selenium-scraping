bda569e332d7bcf58fa80b3c40603c99ab8d7260
MakovM <makovinskiy09@gmail.com>
2025-10-20 19:25:01 +0300

    Solution

--- app/parse.py
+++ app/parse.py
@@ -42,6 +42,7 @@ def get_driver() -> WebDriver:
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     driver = webdriver.Chrome(options=options)
+
     return driver
 
 
